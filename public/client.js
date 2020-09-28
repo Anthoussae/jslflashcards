441c517b8b8c65f13b3c1c97a469bfb2fc935a10
James Stone Lunde <james.stonelunde@gmail.com>
2020-09-27 21:13:17 -0700

    fixed JSON

--- public/client.js
+++ public/client.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded",()=>{
             }
         }).then((res)=>{
             res.json().then((data)=>{
-                console.log(data);
+                console.log(data.value);
             })
         })
     })
